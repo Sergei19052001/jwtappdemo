f7252beeb7c1a16bde23b78640596dac4690c0ab
Sergei19052001 <kudrasovcergej3@gmail.com>
2023-06-05 20:42:40 +0500

    last commit

---
+++ jwtappdemo/src/main/java/net/proselyte/jwtappdemo/dto/AuthenticationRequestDto.java
@@ -0,0 +1,19 @@
+package net.proselyte.jwtappdemo.dto;
+
+public class AuthenticationRequestDto {
+    private String username;
+    private String password;
+
+    public AuthenticationRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
